b28ff561f4ee259fe429bf25d6246f7a480f8671
Code Decode <codergaming25@gmail.com>
2022-12-12 18:34:43 +0530

    fds

--- src/main/java/com/example/demo/controller/EmployeeController.java
+++ src/main/java/com/example/demo/controller/EmployeeController.java
@@ -3,10 +3,7 @@ package com.example.demo.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.example.demo.entity.Employee;
 import com.example.demo.service.EmployeeService;
@@ -14,12 +11,18 @@ import com.example.demo.service.EmployeeService;
 @RestController
 @RequestMapping("/code")
 public class EmployeeController {
-		@Autowired
-		private EmployeeService employeeService;
+    @Autowired
+    private EmployeeService employeeService;
 
-		@PostMapping("/save")
-		public ResponseEntity<?> addEmployee(@RequestBody Employee employee) throws Exception{
-				Employee employeeSaved = employeeService.addEmployee(employee);
-				return new ResponseEntity<Employee>(employeeSaved, HttpStatus.CREATED);
-			}
+    @PostMapping("/save")
+    public ResponseEntity<?> addEmployee(@RequestBody Employee employee) throws Exception {
+        Employee employeeSaved = employeeService.addEmployee(employee);
+        return new ResponseEntity<Employee>(employeeSaved, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/test")
+    public ResponseEntity<?> getEmployee() {
+        return new ResponseEntity<String>("test", HttpStatus.CREATED);
+    }
+
 }
